a7f7f34b26d21127c02ea8c3bac62927284c8180
UmarSupra <UmarAKhan10@hotmail.com>
2023-11-01 17:04:58 +0000

    Modified up to API Calling

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
+const request = require('request');
 
 const PORT = process.env.PORT || 5000;
+
+
+// API Key pk_26a2d6a1506a472ab5c96a0f84489218
+
+request('https://api.iex.cloud/v1/data/core/quote/fb?token=pk_26a2d6a1506a472ab5c96a0f84489218', {json: true}, (err, res, body) => {
+    if (err) {return console.log("Error: " + err);}
+    if (res.statusCode === 200) {
+        console.log(body);
+    }
+});
 
 //Set Handlebars Middleware
 app.engine('handlebars', exphbs.engine());
@@ -18,6 +29,11 @@ app.get('/', (req, res) => {
     res.render('home', {
         stuff: otherstuff
     });
+});
+
+// Create about page route
+app.get('/about.html', (req, res) => {
+    res.render('about');
 });
 
 
