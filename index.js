6be0c86893658394280818890368d466af37293a
UmarSupra <UmarAKhan10@hotmail.com>
2023-11-03 17:20:54 +0000

    Stock App Finished

--- index.js
+++ index.js
@@ -5,18 +5,27 @@ const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path');
 const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
+
+// user body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 // API Key pk_26a2d6a1506a472ab5c96a0f84489218
 
-request('https://api.iex.cloud/v1/data/core/quote/fb?token=pk_26a2d6a1506a472ab5c96a0f84489218', {json: true}, (err, res, body) => {
-    if (err) {return console.log("Error: " + err);}
-    if (res.statusCode === 200) {
-        console.log(body);
-    }
-});
+
+function call_api (finishedAPI, ticker) {
+    request('https://api.iex.cloud/v1/data/core/quote/' + ticker + '?token=pk_26a2d6a1506a472ab5c96a0f84489218', {json: true}, (err, res, body) => {
+        if (err) {return console.log("Error: " + err);}
+        if (res.statusCode === 200) {
+            finishedAPI(body);
+        };
+    });
+}
+
+
 
 //Set Handlebars Middleware
 app.engine('handlebars', exphbs.engine());
@@ -26,9 +35,20 @@ const otherstuff = "hello there, this is other stuff!";
 
 //Set Handlebar Routes
 app.get('/', (req, res) => {
-    res.render('home', {
-        stuff: otherstuff
+    call_api(function(doneAPI) {
+        res.render('home', {
+            stock: doneAPI[0]
+        });
     });
+});
+
+app.post('/', (req, res) => {
+    call_api(function(doneAPI) {
+        //posted_stuff = req.body.stock_ticker;
+        res.render('home', {
+            stock: doneAPI[0],
+        });
+    }, req.body.stock_ticker);
 });
 
 // Create about page route
